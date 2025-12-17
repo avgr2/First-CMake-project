7aae80f32676b28b2d66d853a58ddf505d3d662a
avgr2 <arthur.vigier@ipsa.fr>
2025-12-17 09:01:35 -0600

    First CMake project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <firstConfig.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+
+    cout << argv[0] << " Version" << first_VERSION_MAJOR << "." << first_VERSION_MINOR << endl;
+    cout << " Hello" << endl;
+
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# run.sh
+cd build; ./first
